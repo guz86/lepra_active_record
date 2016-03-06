70346880fcd845f17d593b00414a516872c7fca1
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-06 06:13:20 +0300

    bd_set, class, gemfile

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:lepra.db"
+
+class Posts < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello!"			
